12edc784f4be75fe217c6ce58b3d477fba9047ac
Tarantel <SerienKid@live.de>
2024-08-09 21:51:42 +0200

    Update InfuseStationBlockEntity.java

--- src/main/java/net/trivo/trivocustommod/block/entity/InfuseStationBlockEntity.java
+++ src/main/java/net/trivo/trivocustommod/block/entity/InfuseStationBlockEntity.java
@@ -3,9 +3,13 @@ package net.trivo.trivocustommod.block.entity;
 import cpw.mods.util.Lazy;
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.game.ClientGamePacketListener;
+import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
 import net.minecraft.world.Container;
 import net.minecraft.world.Containers;
 import net.minecraft.world.MenuProvider;
@@ -23,6 +27,7 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.neoforged.neoforge.items.IItemHandler;
 import net.neoforged.neoforge.items.ItemStackHandler;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import net.trivo.trivocustommod.block.custom.InfuseStationBlock;
@@ -38,8 +43,8 @@ public class InfuseStationBlockEntity extends BlockEntity implements MenuProvide
     private static final int INPUT_SLOT = 0;
 
     protected final ContainerData data;
-    private int progress = 0;
-    private int maxprogress = 78;
+    private static int progress = 0;
+    private static int maxprogress = 78;
 
     public InfuseStationBlockEntity(BlockPos pos, BlockState blockState) {
         super(ModBlockEntities.INFUSE_STATION_BE.get(), pos, blockState);
@@ -67,7 +72,11 @@ public class InfuseStationBlockEntity extends BlockEntity implements MenuProvide
             }
         };
     }
-
+    @Override
+    public void onLoad() {
+        super.onLoad();
+        setChanged();
+    }
     public void drops() {
         SimpleContainer inventory = new SimpleContainer(itemHandler.getSlots());
         for (int i = 0; i < itemHandler.getSlots(); i++) {
@@ -100,9 +109,17 @@ public class InfuseStationBlockEntity extends BlockEntity implements MenuProvide
         itemHandler.deserializeNBT(registries, tag);
         progress = tag.getInt("infuse_station_block.progress");
     }
+    public @Nullable IItemHandler getItemHandlerCapability(@Nullable Direction side) {
+        if(side == null)
+            return itemHandler;
 
-    public void tick(Level level1, BlockPos pos, BlockState state1) {
-        if (hasInfuseItem()) {
+        return itemHandler;
+    }
+    public static void tick(Level level1, BlockPos pos, BlockState state1, InfuseStationBlockEntity pEntity) {
+        if(level1.isClientSide()) {
+            return;
+        }
+        if (hasInfuseItem(pEntity)) {
             increaseInfuseProgress();
             setChanged(level1, pos, state1);
 
@@ -115,27 +132,38 @@ public class InfuseStationBlockEntity extends BlockEntity implements MenuProvide
         }
     }
 
-    private void resetProgress() {
+    private static void resetProgress() {
         progress = 0;
     }
 
-    private void giveAbilities() {
+    private static void giveAbilities() {
         Player player = Minecraft.getInstance().player;
 
         player.addEffect(new MobEffectInstance(MobEffects.BLINDNESS, 100, 2));
     }
 
-    private boolean hasInfuseItem() {
-        boolean hasDragonItem = this.itemHandler.getStackInSlot(INPUT_SLOT).getItem() == Items.DRAGON_EGG;
+    private static boolean hasInfuseItem(InfuseStationBlockEntity pEntity) {
+        boolean hasDragonItem = pEntity.itemHandler.getStackInSlot(0).getItem() == Items.DRAGON_EGG;
 
         return hasDragonItem;
     }
 
-    private boolean hasProgressFinished() {
+    private static boolean hasProgressFinished() {
         return progress >= maxprogress;
     }
 
-    private void increaseInfuseProgress() {
+    private static void increaseInfuseProgress() {
         progress++;
+    }
+
+    @Nullable
+    @Override
+    public Packet<ClientGamePacketListener> getUpdatePacket() {
+        return ClientboundBlockEntityDataPacket.create(this);
+    }
+
+    @Override
+    public CompoundTag getUpdateTag(HolderLookup.Provider prov) {
+        return saveWithFullMetadata(prov);
     }
 }
